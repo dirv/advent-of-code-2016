1f77def701fd121e9135b1eb196f07804fa509b2
Daniel Irvine <daniel@danielirvine.com>
2016-12-01 21:16:44 +0000

    Far simpler solution

--- day01/day01.c
+++ day01/day01.c
@@ -1,80 +1,58 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-typedef enum { NORTH, SOUTH, EAST, WEST } direction;
+const int numDirections = 4;
 
-const direction turns[] = { NORTH, WEST, SOUTH, EAST };
-const int numTurns = 4;
+int currentDirection = 0;
+int movedDistances[] = {0, 0, 0, 0};
 
-direction turn(direction currentDirection, int increment) {
-  for( int i = 0; i < numTurns; ++i ) {
-    if( turns[i] == currentDirection ) {
-      int nextPos = i + increment;
-      if( nextPos == -1 ) {
-        return turns[numTurns-1];
-      } else if( nextPos == numTurns ) {
-        return turns[0];
-      } else {
-        return turns[nextPos];
-      }
-    }
+void turnLeft() {
+  if(currentDirection == 0) {
+    currentDirection = numDirections;
   }
-  return NORTH;
+  currentDirection--;
 }
 
-int currentDirection = NORTH;
-int currentVerticalDistance = 0;
-int currentHorizontalDistance = 0;
+void turnRight() {
+  if(++currentDirection == numDirections) {
+    currentDirection = 0;
+  }
+}
 
 void moveDistance(int distance) {
-  switch(currentDirection) {
-    case NORTH:
-      currentVerticalDistance += distance;
-      break;
-    case SOUTH:
-      currentVerticalDistance -= distance;
-      break;
-    case WEST:
-      currentHorizontalDistance -= distance;
-      break;
-    case EAST:
-      currentHorizontalDistance += distance;
-      break;
-  }
+  movedDistances[currentDirection] += distance;
 }
 
 int main(int argc, char** argv) {
   FILE* fp = fopen("input.txt", "r");
-
-  int north = 0;
-  int south = 0;
 
   int c;
   int currentDistance = 0;
   while((c = fgetc(fp)) != EOF) {
     switch(c) {
       case 'L':
-        currentDirection = turn(currentDirection, 1);
+        turnLeft();
         break;
       case 'R':
-        currentDirection = turn(currentDirection, -1);
+        turnRight();
         break;
       case ',':
         moveDistance(currentDistance);
         currentDistance = 0;
         break;
-      case ' ':
-        break;
-      case '\n':
-        break;
       default:
-        currentDistance *= 10;
-        currentDistance += c - '0';
+        if ( c >= '0' && c <= '9') {
+          currentDistance *= 10;
+          currentDistance += c - '0';
+        }
     }
   }
   moveDistance(currentDistance);
 
-  printf("%d\n", abs(currentHorizontalDistance) + abs(currentVerticalDistance));
+  printf("%d\n", movedDistances[0]
+      + movedDistances[1]
+      - movedDistances[2]
+      - movedDistances[3]);
 
   return 0;
 }

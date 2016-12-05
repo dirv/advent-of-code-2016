92bdd78b2874444f15ae64212f21398aa014910d
Daniel Irvine <daniel@danielirvine.com>
2016-12-05 15:22:55 +0000

    First part of day 4

---
+++ day04/day04a.c
@@ -0,0 +1,96 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+typedef struct letter {
+  int count;
+  int ch;
+  struct letter *next;
+} letter;
+
+letter* removeOrCreateLetter(letter *head, int ch) {
+  letter* next = head->next;
+  if(next == NULL) {
+    struct letter *l = (letter*)malloc(sizeof(letter));
+    l->count = 0;
+    l->ch = ch;
+    l->next = NULL;
+    return l;
+  }
+  if (next->ch == ch) {
+    head->next = next->next;
+    return next;
+  }
+  return removeOrCreateLetter(next, ch);
+}
+
+letter* findCorrectPlace(letter* head, letter* l) {
+  letter* next = head->next;
+  if (next == NULL) return head;
+  if (l->count > next->count) {
+    return head;
+  }
+  if(l->count == next->count) {
+    if(l->ch < next->ch) {
+      return head;
+    }
+  }
+  return findCorrectPlace(next, l);
+}
+
+void insertAfter(letter* before, letter* l) {
+  l->next = before->next;
+  before->next = l;
+}
+
+void listToWord(letter *head, char* letters) {
+  letter *current = head->next;
+  for(int i = 0; i < 5; ++i) {
+    letters[i] = current->ch;
+    current = current->next;
+  }
+}
+
+void topFiveLetters(char* s, char* letters) {
+  int position = 0;
+  int ch;
+  letter head = {999, 0, NULL};
+  while((ch = s[position++]) != '\0') {
+    if(ch == '-') {
+      continue;
+    }
+    struct letter *letter = removeOrCreateLetter(&head, ch);
+    letter->count++;
+    struct letter *place = findCorrectPlace(&head, letter);
+    insertAfter(place, letter);
+  }
+  listToWord(&head, letters);
+}
+
+int main(int argc, char** argv) {
+  FILE *fp = fopen("input.txt", "r");
+
+  int result;
+  char name[200];
+  char checksum[5];
+  long sectorIdSum = 0;
+
+  while((result = fscanf(fp, "%[a-z0-9-]\[%5s]\n", name, checksum)) != -1) {
+    int length = strlen(name);
+    char nameOnly[200] = { 0 };
+    char topFive[5];
+    strncpy(nameOnly, name, length - 4);
+    topFiveLetters(nameOnly, topFive);
+
+    if(strncmp(topFive, checksum, 5) == 0)
+    {
+      char *id = strrchr(name, '-') + 1;
+      long sectorId = strtol(id, NULL, 10);
+      sectorIdSum += sectorId;
+    }
+  }
+  printf("%ld\n", sectorIdSum);
+
+  fclose(fp);
+  return 0;
+}
